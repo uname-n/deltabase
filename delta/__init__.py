2118679d098dc01ddb248ca3dfedd0b283d4dad8
dm <118793435+uname-n@users.noreply.github.com>
2024-08-10 20:07:47 -0700

    speeed

--- delta/__init__.py
+++ delta/__init__.py
@@ -1,7 +1,10 @@
-from polars import SQLContext, DataFrame, LazyFrame, read_delta, coalesce, struct
+from types import LambdaType
+
+from polars import SQLContext, DataFrame, LazyFrame, scan_delta, coalesce, struct
 from os.path import join, exists
 from os import listdir
 from datetime import datetime
+
 
 class delta:
     __delta_source:str
@@ -21,12 +24,11 @@ class delta:
         for table in listdir(delta_cls.__delta_source):
             table_path = join(delta_cls.__delta_source, table)
             if exists(table_path):
-                delta_cls.__delta_sql_context.register(table, read_delta(table_path))
+                delta_cls.__delta_sql_context.register(table, scan_delta(table_path))
         
         return delta_cls
-
-    def __sync_data(self, primary_key:str, target_data:LazyFrame, source_data:DataFrame) -> DataFrame:
-        target_data = target_data.collect()
+    
+    def __sync_data(self, primary_key:str, target_data:LazyFrame, source_data:LazyFrame) -> LazyFrame:
         update_data = source_data.join(
             target_data,
             on=primary_key,
@@ -35,51 +37,55 @@ class delta:
             coalesce=True
         )
 
-        coalesce_columns = [col for col in update_data.columns if "_delta_target" in col]
+        update_data_columns = update_data.collect_schema().names()
+
+        coalesce_columns = [col for col in update_data_columns if "_delta_target" in col]
         coalesce_columns_strip = [col.replace("_delta_target", "") for col in coalesce_columns]
 
-        for col in coalesce_columns_strip:
-            update_data = update_data.with_columns(
-                coalesce([f"{col}_delta_target", col]).alias(col)
-            )
-
-        update_data = update_data.drop(coalesce_columns)
+        update_data = update_data.with_columns([
+            coalesce([f"{col}_delta_target", col]).alias(col) 
+            if f"{col}_delta_target" in update_data_columns else update_data[col]
+            for col in coalesce_columns_strip
+        ]).drop(coalesce_columns)
+        
         return update_data
-
+    
     def __add_multiple(self, table:str, primary_key:str, data:list[dict]):
         if table not in self.tables:
-            target_data = DataFrame(data)
+            target_data = LazyFrame(data)
             self.__delta_sql_context.register(table, target_data)
             return target_data
         
         table_path = join(self.__delta_source, table)
         if exists(table_path):
-            source_data = read_delta(table_path)
-            staged_data = self.sql(f"select * from {table}").lazy()
+            source_data = scan_delta(table_path)
+            staged_data = self.sql(f"select * from {table}", lazy=True)
             source_data = self.__sync_data(primary_key, staged_data, source_data)
         else:
-            source_data = self.sql(f"select * from {table}").lazy()
+            source_data = self.sql(f"select * from {table}", lazy=True)
 
-        target_data = DataFrame(data).lazy()
+        target_data = LazyFrame(data)
         update_data = self.__sync_data(primary_key, target_data, source_data)
 
         self.__delta_sql_context.register(table, update_data)
-
-        return update_data
 
     def add(self, table:str, primary_key:str, data:list[dict] | dict):
         if type(data) == dict: return self.__add_multiple(table, primary_key, [data])
         elif type(data) == list: return self.__add_multiple(table, primary_key, data)
         else: raise ValueError(f"'data' was provided '{type(data)}', type must be 'list[dict]' or 'dict'")
 
-    def delete(self, table:str, filter:callable) -> int:
-        source_data:DataFrame = self.sql(f"select * from {table}")     
-        filter_data = source_data.filter(
-            ~struct(source_data.columns).map_elements(filter, return_dtype=bool)
-        )
+    def delete(self, table:str, filter:str|LambdaType):
+        if type(filter) == str:
+            filter_data = self.sql(f"select * from {table} where {filter}", lazy=True)
+        elif type(filter) == LambdaType:
+            source_data = self.sql(f"select * from {table}", lazy=True)
+            filter_data = source_data.filter(
+                ~struct(source_data.collect_schema().names()).map_elements(filter, return_dtype=bool)
+            )
+        else: raise ValueError(f"'filter' was provided '{type(filter)}', type must be 'callable' or 'str'")
+
         self.__delta_sql_context.register(table, filter_data)
-        return len(source_data) - len(filter_data)
-        
+
     def commit(self, table:str, overwrite:bool=False, force:bool=False):
         data = self.sql(f"select * from {table}")
         options = dict(mode="overwrite")
@@ -88,7 +94,8 @@ class delta:
     
     def checkout(self, table:str, version:int|str|datetime):
         table_path = join(self.__delta_source, table)
-        self.__delta_sql_context.register(table, read_delta(table_path, version=version))
+        self.__delta_sql_context.register(table, scan_delta(table_path, version=version))
 
-    def sql(self, query:str) -> DataFrame:
-        return self.__delta_sql_context.execute(query).collect()
+    def sql(self, query:str, lazy:bool=False) -> DataFrame | LazyFrame:
+        if not lazy: return self.__delta_sql_context.execute(query).collect()
+        return self.__delta_sql_context.execute(query)
--- example.py
+++ example.py
@@ -1,14 +1,16 @@
 from time import time
 from random import randint
+from shutil import rmtree
 
 from delta import delta
 
+# rmtree("tutorial.delta")
 db:delta = delta.connect(path="tutorial.delta")
 
 data = lambda w, h: [{"name":f"name_{_}", **{f"field_{n}":n+_ for n in range(w)}} for _ in range(h)]
 
-testing_data = data(100, 1_000_000)
-# testing_data = data(100, 10_000)
+# testing_data = data(100, 1_000_000)
+testing_data = data(10_000, 10_000)
 
 # print("starting")
 # s = time()
@@ -22,15 +24,26 @@ testing_data = data(100, 1_000_000)
 
 # print(db.sql("select * from table_name"))
 
-
 print("starting")
 s = time()
 # ---
 db.add(table="table_name", primary_key="name", data=testing_data)
 db.commit("table_name")
-db.delete("table_name", lambda row: (row["field_69"] % 2) == 0 )
+db.delete("table_name", "field_6 % 2 == 0")
 db.commit("table_name")
+
+print(db.sql("select * from table_name"))
 db.checkout("table_name", version=0)
 print(db.sql("select * from table_name"))
 # ---
 print(f"{time()-s:.2f} seconds.")
+
+# print("starting")
+# s = time()
+# # ---
+# db.checkout("table_name", version=0)
+# db.delete("table_name", "field_6 % 2 == 0")
+# # db.delete("table_name", lambda row: (row["field_6"] % 2) == 0 )
+# print(db.sql("select * from table_name"))
+# # ---
+# print(f"{time()-s:.2f} seconds.")
