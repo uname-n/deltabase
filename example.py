215c3b9f8e92adb997f7cf6b677c57118d5fbde6
dm <118793435+uname-n@users.noreply.github.com>
2024-08-10 17:17:36 -0700

    example

--- example.py
+++ example.py
@@ -5,45 +5,17 @@ from delta import delta
 
 db:delta = delta.connect(path="tutorial.delta")
 
-data = lambda w, h: [{f"field_{n}":randint(1,10) for n in range(w)} for _ in range(h)]
+data = lambda w, h: [{"name":f"name_{_}", **{f"field_{n}":randint(1,10) for n in range(w)}} for _ in range(h)]
 
+testing_data = data(100, 1_000_000)
+
+s = time()
 # ---
-start = time()
-print(db.add(table="table_name", primary_key="name", data=data(100, 1_000)))
-print(time()-start)
+db.add(table="table_name", primary_key="name", data=testing_data)
+db.commit("table_name")
+# db.delete("table_name", lambda row: (row["field_69"] % 2) == 0 )
+db.sql("select field_43 from table_name where field_43 = 1")
 # ---
+print(f"{time()-s:.2f} seconds.")
 
-# ---
-start = time()
-print(db.delete("table_name", lambda row: (row["field_69"] % 2) == 0 ))
-print(time()-start)
-# ---
-
-# ---
-start = time()
-print(db.sql("select field_43 from table_name where field_43 = 1"))
-print(time()-start)
-# ---
-
-# from json import dumps
-# print(dumps(data(100, 5), indent=4))
-
-# start = time()
-# print(db.add(table="table_name", primary_key="name", data=[
-#     dict(name=f"name_{n}", id=n) for n in range(1_000_000)
-# ]))
-# print(time()-start)
-
-# db.commit("table_name")
-
-# start = time()
-# print(db.sql("select * from table_name where id = 427635"))
-# print(time()-start)
-
-# start = time()
-# print(db.delete("table_name", lambda row: (row["id"] % 2) == 0 ))
-# print(time()-start)
-
-# start = time()
-# print(db.sql("select * from table_name"))
-# print(time()-start)
+print(db.sql("select * from table_name"))
